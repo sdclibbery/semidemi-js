030afeda50dc9f04eaeeda8acb377a77f0a1cbc2
Stephen Clibbery <stephen.clibbery@bbc.co.uk>
2014-12-22 11:59:54 +0000

    Add matchers for chrome and a samsung tv

--- src/identify.js
+++ src/identify.js
@@ -1,7 +1,15 @@
 var SemiDemi = (function (SemiDemi) {
 
+  var matchers = [
+    [ { brand: "google", model: "chrome" }, { fuzzy: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) " }, { invariant: "Chrome" }, { fuzzy: "/39.0.2171.71 Safari/537.36" } ],
+    [ { brand: "samsung", model: "pe5" }, { fuzzy: "Mozilla/5.0 (SmartHub; " }, { invariant: "SMART-TV" }, { fuzzy: "; U; Linux/SmartTV; " }, { invariant: "Maple2012" }, { fuzzy: ") AppleWebKit/534.7 (KHTML, like Gecko) SmartTV Safari/534.7 " }, { invariant: "Series=5000(TV)" } ]
+  ];
+
   SemiDemi.identify = function (ua) {
-    return "hello";
+    if (ua === undefined) {
+      ua = navigator.userAgent;
+    }
+    return SemiDemi.bestMatch(matchers, ua);
   };
 
   return SemiDemi;
