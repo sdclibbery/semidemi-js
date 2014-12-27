c943e37592b5baa2003cbf4d0a4294310a2318da
sdclibbery <sdclibbery@googlemail.com>
2014-12-27 15:45:54 +0000

    Parse overall top level structure

--- spec/parser-spec.js
+++ spec/parser-spec.js
@@ -1,8 +1,15 @@
 describe("Parser", function() {
 
-
-  it("empty file produces no matchers", function() {
+  it("handles simple fuzzy matcher", function() {
+    expect(SemiDemi.parse("brand model: abc")).toEqual([ [ {brand: "brand", model: "model"}, {fuzzy: "abc"} ] ]);
+  });
+/*
+  it("produces no matchers for an empty file", function() {
     expect(SemiDemi.parse("")).toEqual([ ]);
   });
 
+  it("handles various whitespace between sections", function() {
+    expect(SemiDemi.parse("brand \tmodel  \t:\t abc")).toEqual([ [ {brand: "brand", model: "model"}, {fuzzy: "abc"} ] ]);
+  });
+*/
 });
--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,21 @@
 var SemiDemi = (function (SemiDemi) {
 
-  SemiDemi.parse = function (file) {
-    return [];
+  SemiDemi.parse = function (input) {
+    var matchers = [];
+    matchers.push(parseMatcher(input));
+    return matchers;
+  };
+
+  var topRegex = /\s*([^\s]+)\s+([^:]+):\s*(.*)/;
+  var parseMatcher = function (input) {
+    var sections = input.match(topRegex);
+    var brand = sections[1];
+    var model = sections[2];
+    return [
+      { brand: brand, model: model },
+      { fuzzy: sections[3] } // Temp
+    ];
   };
 
   return SemiDemi;
-
 }) ( SemiDemi || {} );
