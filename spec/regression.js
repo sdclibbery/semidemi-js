8ed85aac4210817b1f875297b2ca08b9910958ea
sdclibbery <sdclibbery@googlemail.com>
2014-12-28 23:42:56 +0000

    Better tests

--- spec/regression.js
+++ spec/regression.js
@@ -1,41 +1,59 @@
 
-var logDirect = function (str) { document.getElementById("output").innerHTML += str; };
-var log = function (str) { logDirect("<p>"+str+"</p>"); };
+var escapeHtml = function (unsafe) {
+  return unsafe
+       .replace(/&/g, "&amp;")
+       .replace(/</g, "&lt;")
+       .replace(/>/g, "&gt;")
+       .replace(/"/g, "&quot;")
+       .replace(/'/g, "&#039;");
+}
+var logDirect = function (type, str) { document.getElementById(type).innerHTML += str; };
+var log = function (type, str) { logDirect(type, "<p>"+escapeHtml(str)+"</p>"); };
 
 
-log("Loaded " + tests.length + " tests");
+log("info", "Loaded " + tests.length + " tests");
 
 // Parse demi file
 var matchers = SemiDemi.parse(demiFile);
-log("Loaded " + matchers.length + " matchers");
+log("info", "Loaded " + matchers.length + " matchers");
 
 // Run the tests
-logDirect("<p>");
-for (var i=0; i < tests.length; i++) {
+var runTest = function (i) {
   var result = SemiDemi.bestMatch(matchers, tests[i].uagent);
-  if (!result) {
-    logDirect("x ");
-    var msg = "<p>No match found for: "+tests[i].uagent;
-    msg += "<br />Expected: "+expected+"</p>";
-    document.getElementById("errors").innerHTML += msg;
-    continue;
-  }
   var expected = tests[i].wurfl_id;
+  if (!result) {
+    logDirect("results", "x ");
+    var msg = "No match found for: "+tests[i].uagent;
+    msg += "       Expected: "+expected+"";
+    log("errors", msg);
+    return;
+  }
   var actual = result[0].brand+"_"+result[0].model;
   if (expected.toLowerCase() === actual.toLowerCase()) {
-    logDirect(". ");
+    logDirect("results", ". ");
   } else {
-    logDirect("x ");
-    var msg = "<p>FAIL: "+tests[i].uagent;
+    logDirect("results", "x ");
+    var msg = "FAIL: "+tests[i].uagent;
     if (result) {
-      msg += "<br />Expected: "+expected;
-      msg += "<br />Actual: "+actual;
+      msg += "     Expected: "+expected;
+      msg += "     Actual: "+actual;
     }
-    msg += "</p>";
-    document.getElementById("errors").innerHTML += msg;
+    log("errors", msg);
   }
 }
-logDirect("</p>");
+
+var executeTest = function () {
+  for (var i = 0; i < 10; i++) {
+    runTest(testNumber);
+    testNumber++;
+    if (testNumber >= tests.length) { break; }
+  }
+  if (testNumber < tests.length) {
+    setTimeout(executeTest, 0);
+  }
+}
+var testNumber = 0;
+setTimeout(executeTest, 0);
 
 
 // Output the results
