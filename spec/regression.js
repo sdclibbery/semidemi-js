ff0a1fb037e19ff7fb9877880c74321c1bcdb3c0
sdclibbery <sdclibbery@googlemail.com>
2014-12-28 22:39:09 +0000

    Load regression test data

--- spec/regression.js
+++ spec/regression.js
@@ -3,6 +3,8 @@ var log = function (str) {
   document.getElementById("output").innerHTML += "<p>"+str+"</p>";
 };
 
+log("Loaded " + testFile.length + " tests");
+
 // Parse demi file
 var matchers = SemiDemi.parse(demiFile);
 log("Loaded " + matchers.length + " matchers");
