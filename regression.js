9a7f6d97d998cec59ff67e4a9718eb0328c208be
sdclibbery <sdclibbery@googlemail.com>
2015-01-31 10:25:59 +0000

    fixes

--- regression.js
+++ regression.js
@@ -50,8 +50,8 @@ var runTest = function (testdata) {
 }
 
 function runTests (tests) {
-  var start = 960;//process.argv[2] || 0;
-  var end = 980;//process.argv[3] || tests.length;
+  var start = 970;//process.argv[2] || 0;
+  var end = 985;//process.argv[3] || tests.length;
   console.log("Num UAs: " + tests.length);
   for (var i = start; i < end; i++) {
     runTest(tests[i]);
